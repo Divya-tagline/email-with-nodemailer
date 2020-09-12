b4d518ae0ffe606a769dd8c5fa77e94cbdc0d5bb
divya gadhiya <divyaa.tagline@gmail.com>
2020-09-12 12:48:36 +0530

    2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+const cron = require("node-cron");
+const fs = require("fs");
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -22,6 +24,22 @@ var mailOptions = {
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/cron', function(req, res, next) {
+  cron.schedule("* * * * *", function() {
+    console.log("running a task every minute");
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.send(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.send('Email sent: ' + info.response);
+      }
+    });
+  });
+});
+
 
 router.get('/emailsend', function(req, res, next) {
   transporter.sendMail(mailOptions, function(error, info){
